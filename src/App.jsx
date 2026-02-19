6a881bae888bd5cbc6dde5f1d21068eb7e9a671a
rubenmasternuclio <rubenmasternuclio@gmail.com>
2026-02-19 13:35:41 +0100

    punto de control antes de implementar login de employee

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import Navbar from './components/Navbar/Navbar'
 import LoginCompanyPage from './pages/LoginCompanyPage'
+import DesignSystem from './pages/DesignSystem'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/login-company' element={<LoginCompanyPage />} />
+          <Route path='/design-system' element={<DesignSystem />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav>
             <ul className="flex gap-4 py-10">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                <li><Link className="text-white px-4 py-2 rounded" to="/register">Register</Link></li>
                 <li><Link to="/login-company">Login Company</Link></li>
             </ul>
         </nav>
