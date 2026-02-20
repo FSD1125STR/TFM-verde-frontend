21e70e04677b56241cf641241245044f327e0f62
rubenmasternuclio <rubenmasternuclio@gmail.com>
2026-02-20 16:27:11 +0100

    se añade funcionalidad de login, con control de sesion en loginContext y se hace traen detos del profile (este sin terminar)

---
+++ src/services/fetchProfileEmployee.js
@@ -0,0 +1,23 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+
+
+export const fetchProfileEmployee = async () => {
+   
+    const response = await fetch(`${API_BASE_URL}/employee/profile`, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+        credentials: "include",
+    });
+
+    if (response.status === 401) return null;
+
+    const responseData = await response.json();
+
+    if(!response.ok) {
+        throw new Error(`${responseData.message}`);
+    }
+
+    return responseData;
+
+
+};   
