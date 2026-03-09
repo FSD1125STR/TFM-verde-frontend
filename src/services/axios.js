5e5f6113960115bee626d31fa8c9c190b84a3977
Alejandro <66677531+Abanoral@users.noreply.github.com>
2026-03-09 16:28:08 +0100

    Cloudinary endpoins

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api', // Cambia esto a la URL de tu backend
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    });
+  baseURL: 'http://localhost:4000/api', // Cambia esto a la URL de tu backend
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 export default instance;
