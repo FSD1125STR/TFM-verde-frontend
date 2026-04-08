df4bfdda60578889e7fc2fc34105acfc88e8a074
bastianmolinadiaz1710-tech <bastianmolinadiaz1710@gmail.com>
2026-04-08 15:55:31 +0200

    Revert "Feature/vehicle reception flujo recepción de vehículo completo"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,5 @@
 export default function HomePage() {
     return (
-
-        <>
-            <h1 className="text-3xl font-bold">Home Page</h1>
-
-            <select defaultValue="Pick a color" className="select">
-                <option disabled={true}>Pick a color</option>
-                <option>Crimson</option>
-                <option>Amber</option>
-                <option>Velvet</option>
-            </select>
-        </>
-
-
+        <h1 className="text-3xl font-bold">Home Page</h1>
     )
 }
