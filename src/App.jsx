7e3705e3128d7400b2dc12ad31b995fc42cca23b
Alejandro <66677531+Abanoral@users.noreply.github.com>
2026-04-17 20:01:22 +0200

    app.jsx loginProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import RegisterPage from './pages/RegisterPage'
-import Navbar from './components/Navbar/Navbar'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
-import DesignSystem from './pages/DesignSystem'
-import { LoginProvider } from './contexts/LoginContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import Navbar from './components/Navbar/Navbar';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import DesignSystem from './pages/DesignSystem';
+import { LoginProvider } from './contexts/LoginProvider';
 
 function App() {
-
   return (
     <>
       <LoginProvider>
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/register' element={<RegisterPage />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/profile' element={<ProfilePage />} />
             <Route path='/design-system' element={<DesignSystem />} />
@@ -24,7 +23,7 @@ function App() {
         </BrowserRouter>
       </LoginProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
