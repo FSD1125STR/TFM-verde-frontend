f87c2122a205ee8869b251ef7620108d7e600208
Bastian Molina <bastianmolinadiaz1710@gmail.com>
2026-04-21 18:44:38 +0200

    app y sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,63 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; import HomePage from './pages/HomePage'; import RegisterPage from './pages/RegisterPage'; import LoginPage from './pages/LoginPage'; import ProfilePage from './pages/ProfilePage'; import DesignSystem from './pages/DesignSystem'; import VehicleReceptionPage from './pages/VehicleReceptionPage'; import ClientInfoPage from './pages/ClientInfoPage'; import VehicleStatusPage from './pages/VehicleStatusPage'; import VehicleSignaturePage from './pages/VehicleSignaturePage'; import EmployeePage from './pages/EmployeePage'; import AppLayout from './components/layout/AppLayout'; import { LoginProvider } from './contexts/LoginProvider.jsx'; import WorkOrdersPage from './pages/WorkOrdersPage'; function App() { return ( <BrowserRouter> <LoginProvider> <Routes> <Route path='/login' element={<LoginPage />} /> <Route path='/register' element={<RegisterPage />} /> <Route element={<AppLayout />}> <Route path='/work-orders' element={<WorkOrdersPage />} /> <Route path='/' element={<HomePage />} /> <Route path='/employees' element={<EmployeePage />} /> <Route path='/profile' element={<ProfilePage />} /> <Route path='/design-system' element={<DesignSystem />} /> <Route path='/vehicle-reception' element={<VehicleReceptionPage />} /> <Route path='/client-info' element={<ClientInfoPage />} /> <Route path='/vehicle-status' element={<VehicleStatusPage />} /> <Route path='/vehicle-signature' element={<VehicleSignaturePage />} /> </Route> </Routes> </LoginProvider> </BrowserRouter> ); } export default App;
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import DesignSystem from "./pages/DesignSystem";
+import VehicleReceptionPage from "./pages/VehicleReceptionPage";
+import ClientInfoPage from "./pages/ClientInfoPage";
+import VehicleStatusPage from "./pages/VehicleStatusPage";
+import VehicleSignaturePage from "./pages/VehicleSignaturePage";
+import EmployeePage from "./pages/EmployeePage";
+import WorkOrdersPage from "./pages/WorkOrdersPage";
+import ChatPage from "./pages/ChatPage";
+import AppLayout from "./components/layout/AppLayout";
+import { LoginProvider } from "./contexts/LoginProvider.jsx";
+import { LoginContext } from "./contexts/AuthContext.js";
+
+function AppRoutes() {
+  const { isAuthenticated } = useContext(LoginContext);
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
+      />
+
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route
+        path="/"
+        element={isAuthenticated ? <AppLayout /> : <Navigate to="/login" />}
+      >
+        <Route index element={<HomePage />} />
+        <Route path="employees" element={<EmployeePage />} />
+        <Route path="vehicle-reception" element={<VehicleReceptionPage />} />
+        <Route path="client-info" element={<ClientInfoPage />} />
+        <Route path="vehicle-status" element={<VehicleStatusPage />} />
+        <Route path="vehicle-signature" element={<VehicleSignaturePage />} />
+        <Route path="work-orders" element={<WorkOrdersPage />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="design-system" element={<DesignSystem />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <LoginProvider>
+        <AppRoutes />
+      </LoginProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
